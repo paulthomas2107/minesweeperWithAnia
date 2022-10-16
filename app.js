a562cd2df5050a00ca214017214254cc4730da49
Paul Thomas <paulthomas2107@gmail.com>
2022-10-16 14:05:08 +0100

    Add win

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
         square.classList.add('flag')
         square.innerHTML = ' 🚩'
         flags++
+        checkForWin()
       } else {
         square.classList.remove('flag')
         square.innerHTML = ''   
@@ -176,5 +177,18 @@ document.addEventListener('DOMContentLoaded', () => {
         square.innerHTML = '💣';
       }
     });
+  }
+  
+  // Check for win 
+  function checkForWin() {
+    let matches = 0
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i].classList.contains('flag') && squares[i].classList.contains('bomb')) {
+            matches++   
+        }
+        if (matches === bombAmount) {
+            console.log("You win !!!!")
+        }
+    }
   }
 });
